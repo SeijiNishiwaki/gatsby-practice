5f9530a08f3f380a8463fb6cb80cec6d9c90fb42
Seiji Nishiwaki <nishiwakiat.wjnisi@gmail.com>
2020-07-06 01:53:26 +0900

    チュートリアル

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <Link to="/contact/">Contact</Link>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
